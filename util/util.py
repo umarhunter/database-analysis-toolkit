46d32003da3e46d6d076bde277a3ee4b1fc0b1e6
umarhunter <markufaruque@gmail.com>
2024-08-18 13:56:39 -0400

    Add helper functions for saving different file types

--- util/util.py
+++ util/util.py
@@ -1,8 +1,46 @@
 # Utility file (util.py)
+import os
+
 
 def get_features(data):
     return data.columns.tolist()
 
 
-def save_csv(df, output_file):
-    df.to_csv(output_file, index=False)
+def save_csv(df, filename, file_type='.csv'):
+    directory = 'results/'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    file_path = directory + filename + file_type
+    df.to_csv(file_path, index=False)
+
+
+def save_excel(df, filename):
+    directory = 'results/'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    file_path = os.path.join(directory, filename + '.xlsx')
+    df.to_excel(file_path, index=False)
+
+
+def save_json(df, filename):
+    directory = 'results/'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    file_path = os.path.join(directory, filename + '.json')
+    df.to_json(file_path, orient='records', lines=True)
+
+
+def save_parquet(df, filename):
+    directory = 'results/'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    file_path = os.path.join(directory, filename + '.parquet')
+    df.to_parquet(file_path, index=False)
+
+
+def save_feather(df, filename):
+    directory = 'results/'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    file_path = os.path.join(directory, filename + '.feather')
+    df.to_feather(file_path)
